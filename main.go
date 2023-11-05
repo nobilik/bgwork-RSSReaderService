691455ae5c58abf3b1e898146abfcd80a3b54043
nobilik <kkurshin@mail.ru>
2023-11-05 17:18:31 +0700

    change_for_docker

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
+	if os.Getenv("PORT") != "" {
+		return
+	}
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
